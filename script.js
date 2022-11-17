46d1372d062000d6470dfb0f817e2bcb68239c69
Skryre <zbk1998@ymail.com>
2022-11-17 08:46:50 -0500

    intial code for my constructor and the array which gonna save the books, plus the html frame.

--- script.js
+++ script.js
@@ -1,12 +1,13 @@
-function book(title, author, pages, status) {
-    this.title = title
-    this.author = author
-    this.pages = pages
-    this.status = status
-    this.sayTitle = function() {
-      console.log(title, author, pages, status)
-    }
-  }
+let myLibrary = [];
 
-const book1 = new book('the hobbit', 'tolkien', '230', 'not finished')
-book1.sayTitle()
+function Book(title, author, pages) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+}
+
+function addBookToLibrary() {
+    const book = new Book(title, author, pages);
+    return myLibrary.push(book)
+}
+
